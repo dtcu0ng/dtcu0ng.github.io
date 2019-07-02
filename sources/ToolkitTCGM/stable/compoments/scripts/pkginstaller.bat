2c3cd3e78fc09c79ee31842e763f75f924933810
dtcuong <34135258+CuongZ@users.noreply.github.com>
2019-07-02 21:43:29 +0700

    gg

---
+++ sources/ToolkitTCGM/stable/compoments/scripts/pkginstaller.bat
@@ -0,0 +1,33 @@
+color 3f
+title Package Installer - Toolkit for Tencent Gaming Buddy
+cls
+@echo off
+echo First, you need make sure you've enable developer setting and USB debugging like in this video: https://youtu.be/ksQB_eniDYU
+pause
+cls
+@echo off
+echo Make sure you have downloaded APK to install ! And put it in the same location the file Tookit.bat
+pause
+cls
+@echo off
+echo Now you can see your emulator with named ' emulator5554 emulator '?
+@echo off
+echo If it not displayed, please check the step above again. If it displayed, press any key to continue.
+cd ..
+cd ..
+compoments\adb kill-server
+compoments\adb devices
+pause
+cls
+set appname= Default
+@echo off
+echo Please enter your app name! 
+set /p appname=
+@echo off
+echo Now installing your app ( %appname%.apk )
+compoments\adb install ./%appname%.apk
+@echo off
+echo Finished app install ! Enjoy your app !
+pause
+cd compoments\scripts\
+call selector.bat
